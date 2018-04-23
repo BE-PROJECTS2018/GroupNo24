f0ef5de89460b57b5334e511f2c521a4d9a29824
codeplay13 <muzic.maniac7@gmail.com>
2018-04-23 21:06:46 +0530

    Add files via upload

---
+++ Source Code/download_model.py
@@ -0,0 +1,17 @@
+import six.moves.urllib as urllib
+import sys
+
+# This is needed since the notebook is stored in the object_detection folder.
+sys.path.append("..")
+
+# What model to download.
+MODEL_NAME = 'ssd_mobilenet_v1_coco_2017_11_17'
+MODEL_FILE = MODEL_NAME + '.tar.gz'
+DOWNLOAD_BASE = 'http://download.tensorflow.org/models/object_detection/'
+
+# List of the strings that is used to add correct label for each box.
+
+print("[INFO] Downloading model...\nThis may take long time due to large file size")
+opener = urllib.request.URLopener()
+opener.retrieve(DOWNLOAD_BASE + MODEL_FILE, MODEL_FILE)
+print("[INFO] Download Complete!!")
